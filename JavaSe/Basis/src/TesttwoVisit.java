73a2df9f0df181c28ebd2ca839812ddccbc7a08c
wubaolei <1339468596@qq.com>
2018-11-12 11:05:54 +0800

    封装

--- JavaSe/Basis/src/TesttwoVisit.java
+++ JavaSe/Basis/src/TesttwoVisit.java
@@ -2,7 +2,10 @@
  * 1.在Testtwo类中自己访问自己
  * 2.在另外一个类中 访问Testtwo类中的属性
  * 
- * */
+ * 
+ * 
+ * 
+ */
 
 public class TesttwoVisit
 {
@@ -12,7 +15,8 @@ public class TesttwoVisit
 		Testtwo b = new Testtwo();		// 实例化
 		b.setSage(20);					// set设置值
 		b.setSname("烟花易冷");			// set设置值
-		System.out.println("名字为：" + b.getSname() +"，年龄为：" + b.getSage());  // 在输出值中 则为 get 获取值
+		Testtwo.cityname = "长安";		// 原类设置的静态值无法传递，需重新设置
+		System.out.println("名字为：" + b.getSname() +"，年龄为：" + b.getSage() + "，所在城市：" + b.cityname);  // 在输出值中 则为 get 获取值
 	}
 	
 }
