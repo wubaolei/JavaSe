fc1546d2427ef6a5f99b6ca3cba704dabfcb47bc
wubaolei <1339468596@qq.com>
2018-11-13 13:17:24 +0800

    生命周期

--- JavaSe/Basis/src/Testthree.java
+++ JavaSe/Basis/src/Testthree.java
@@ -1,8 +1,26 @@
 public class Testthree
 {
+
+	String a;
+
+	static int b;
+
 	static		// 静态代码块
 	{
+		b = 10;						// 直接访问静态变量
+
+		//a = "10";					// 实例变量 访问报错
+		Testthree t = new Testthree();
+		t.a = "10";					// 实例化对象 才能访问实例变量
+		
 		System.out.println("静态代码块，我是最先执行的，只执行一次");
+		
+/*
+ * 这里的执行顺序
+ * 1.执行静态变量
+ * 2.静态变量里面有实例化对象
+ * 3.实例化对象先执行构造代码块
+ * */
 	}
 	
 	{			// 构造代码块
@@ -13,21 +31,30 @@ public class Testthree
 	{
 		System.out.println("构造函数");
 	}
-	
+
 	public void show()			// 实例方法
 	{
 		System.out.println("实例方法");
 	}
 	public static void check()  // 静态方法 ( 因为有static )
 	{
+		b = 20;		// 直接访问静态变量
+		
+		//a = "10";   // 实例变量 访问报错
+		Testthree t = new Testthree();
+		t.a = "10";	  // 实例化对象 才能访问实例变量
+
 		System.out.println("静态方法");
 	}
-	
+
 	public static void main(String[] args)
 	{
 /*
- * 执行顺序：除开 静态代码块 和 构造代码块 其他的都是按自上而下顺序执行
- * 
+ * 类的生命周期：1.main是入口函数，按照顺序执行
+ * 				2.静态代码块最先执行，并且只执行一次，不需要调用
+ * 				3.按照代码的顺序执行，创建对象时，如果有匿名代码块，最先执行匿名代码块，再执行构造方法
+ * 				4.访问机制，静态代码块 和 静态方法可以直接访问静态变量，不能直接访问实例变量，否则得实例化对象，才能访问
+ * 				5.
  */
 		Testthree a = new Testthree();
 		a.check();
