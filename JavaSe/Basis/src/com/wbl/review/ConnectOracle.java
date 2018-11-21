61b3bd02102e9816c19c226482163759a7e71716
wubaolei <1339468596@qq.com>
2018-11-21 17:14:03 +0800

    连接Oracle

--- JavaSe/Basis/src/com/wbl/review/ConnectOracle.java
+++ JavaSe/Basis/src/com/wbl/review/ConnectOracle.java
@@ -2,17 +2,21 @@ package com.wbl.review;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class ConnectOracle
 {	
-	void aaa()
+	Connection conn;
+	
+	ConnectOracle()
 	{
 		try
 		{
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			
-			Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:1521:XE", "student", "student");
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:1521:XE", "student", "student");
 			
 			System.out.println("ÔÙ¿´¿´£¿-->" + conn);
 		}
@@ -26,14 +30,40 @@ public class ConnectOracle
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	
+	public void selectOracle()
+	{
+		String sql = "select * from pra";
+		
+		try
+		{
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			
+			ResultSet rs = pstmt.executeQuery();
+			
+			while(rs.next())
+			{
+				System.out.println(rs.getInt(1) + "," + rs.getString(2));
+			}
+		}
+		catch (SQLException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		
 	}
+	
 	
 	public static void main(String[] args)
 	{
 		ConnectOracle t = new ConnectOracle();
 		
-		t.aaa();
+		t.selectOracle();
+
 	}
 	
 	
